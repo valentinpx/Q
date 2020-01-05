4c598a3fb48d4c6ed40dfbde83d926c48576d46c
Valentin Sene <valentin.sene@epitech.eu>
2020-01-05 22:18:34 +0100

    * index.html includes axios to make api requests * client.js added a function to get a random q

--- js/client.js
+++ js/client.js
@@ -1,15 +1,29 @@
+let api_path = "http://localhost:8888"
+
 new Vue({
     el: '#app',
     data: {
-        id: 1,
-        url: "https://cdn.dribbble.com/users/1913212/screenshots/6414669/01.jpg",
-        upvotes: 0,
-        downvotes: 0
+        id: null,
+        url: null,
+        upvotes: null,
+        downvotes: null
     },
     methods: {
+        get_random() {
+            axios.get(api_path.concat("/api/random-q"))
+            .then(response => {
+                this.id = response.data.id
+                this.url = response.data.url
+                this.votes.up = response.data.upvotes
+                this.votes.down = response.data.downvotes
+                console.log(this.id)
+            })
+        },
         vote(id, up) {
-            console.log(id)
             console.log(up)
         }
+    },
+    mounted: function() {
+        this.get_random()
     }
 })
